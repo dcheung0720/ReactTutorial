117611dea4f1f0c45f620825b3183f4ec56cdd3f
Railgun707 <73008040+Railgun707@users.noreply.github.com>
2022-04-04 15:34:58 -0500

    schedule removed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-const schedule = {
-  "title" : "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "id" : "F101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "id" : "F110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "id" : "S313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "id" : "S314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
 
 const Banner = ({ title }) => (
     <h1>{ title }</h1>
