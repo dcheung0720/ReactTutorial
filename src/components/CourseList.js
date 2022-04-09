5d003aa96a40ec3bae30e6df878af1af447f7142
Railgun707 <73008040+Railgun707@users.noreply.github.com>
2022-04-09 15:13:30 -0500

    write to database

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Course from './Course.js';
 
+const scheduleChanged = (selected, courses) => (
+  selected.some(course => course !== courses[course.id])
+);
+
 const CourseList = ({ courses }) => {
     const [term, setTerm] = useState('Fall');
     const [selected, setSelected] = useState([]);
+    if (scheduleChanged(selected, courses)) {
+      setSelected([])
+    };
     const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
     return(<>
       <TermSelector term = {term} setTerm={setTerm}/>
